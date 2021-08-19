4d53edd40108286d61a0b246c19a84cb943b4e63
MJKim99 <9mjkim@gmail.com>
2021-08-20 00:47:12 +0900

    header 수정 및 review.html 작성
    반응형 header의 nav 수정
    - 데스크탑 버전 slideToggle -> 모바일 버전 slideToggle 변경
    
    review.html 내용 작성

--- myweb/js/script.js
+++ myweb/js/script.js
@@ -43,11 +43,11 @@ function displayPlaces(places) { // 검색 결과 목록과 마커를 표출
       marker = addMarker(placePosition, i),
       itemEl = getListItem(i, places[i]); // 검색 결과 항목 Element를 생성합니다
 
-    // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
+    // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기 위해
     // LatLngBounds 객체에 좌표를 추가합니다
     bounds.extend(placePosition);
 
-    // 마커와 검색결과 항목에 mouseover 했을때
+    // 마커와 검색결과 항목에 mouseover 했을 때
     // 해당 장소에 인포윈도우에 장소명을 표시합니다
     // mouseout 했을 때는 인포윈도우를 닫습니다
     (function (marker, title) {
